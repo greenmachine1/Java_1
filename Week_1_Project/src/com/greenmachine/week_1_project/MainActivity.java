debd8d325722f695fec44a4c0cb21a981de45fdb
Cory <greenmachine@fullsail.edu>
2013-06-06 01:27:03 -0700

    testing my commits

--- Week_1_Project/src/com/greenmachine/week_1_project/MainActivity.java
+++ Week_1_Project/src/com/greenmachine/week_1_project/MainActivity.java
@@ -10,6 +10,19 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 	@Override
